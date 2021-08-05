1b676bea35cf3f3201fd641149a2681b15f8d880
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-08-05 19:45:06 +0200

    const res = await fetch("https://hp-api.herokuapp.co m/api/characters");

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-
-const charactersList = document.getElementById('charactersList');
-const searchBar = document.getElementById('searchBar');
-// let hpCharacters = [];
+const charactersList = document.getElementById("charactersList");
+const searchBar = document.getElementById("searchBar");
+let hpCharacters = [];
+console.log(searchBar);
+searchBar.addEventListener("keyup", (e) => {
+  console.log(e.target.value);
+});
 
 const loadCharacters = async () => {
-    try {
-        const res = await fetch('https://hp-api.herokuapp.com/api/characters');
-        hpCharacters = await res.json();
-        displayCharacters(hpCharacters);
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const res = await fetch("https://hp-api.herokuapp.co m/api/characters");
+    hpCharacters = await res.json();
+    displayCharacters(hpCharacters);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const displayCharacters = (characters) => {
-    const htmlString = characters
-        .map((character) => {
-            return `
+  const htmlString = characters
+    .map((character) => {
+      return `
             <li class="character">
                 <h2>${character.name}</h2>
                 <p>House: ${character.house}</p>
                 <img src="${character.image}"></img>
             </li>
         `;
-        })
-        .join('');
-    charactersList.innerHTML = htmlString;
+    })
+    .join("");
+  charactersList.innerHTML = htmlString;
 };
 
 loadCharacters();
